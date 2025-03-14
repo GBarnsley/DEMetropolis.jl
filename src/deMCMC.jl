8737c188d339e60477b5fa646965d3850e37ef9a
GBarnsley <gregbarnsley@hotmail.co.uk>
2025-03-14 13:52:07 +0000

    Update deMCMC module: add sample setup and update functions, enhance random number generation, and improve chain update logic

--- src/deMCMC.jl
+++ src/deMCMC.jl
@@ -1,6 +1,6 @@
 module deMCMC
-import Random
 export run_deMCMC
+import Random
 
 struct deMCMC_params
     βs::Array{Float64, 4}
@@ -27,6 +27,18 @@ function select_element(object, iteration, generation, chain, params)
     object[iteration, generation, chain, params]
 end
 
+function setup_samples(iterations, chains, params)
+    (
+        Array{Float64, 3}(undef, length(iterations), length(chains), length(params)),
+        Array{Float64, 2}(undef, length(iterations), length(chains))
+    )
+end
+
+function update_sample!(samples, sample_ld, X, X_ld, it)
+    samples[it, :, :] .= X;
+    sample_ld[it, :] .= X_ld;
+end
+
 function deMCMC_params(iterations, iteration_generation, chains, params, β, rng)
 
     #random β values
@@ -39,35 +51,31 @@ function deMCMC_params(iterations, iteration_generation, chains, params, β, rng
 
     #random chain draws
     chain_draws_1 = generate_random_numbers(rng, iterations, iteration_generation, chains, S = 1:(length(chains) - 1));
-    for i in axes(chain_draws_1, 3)
-        chain_draws_1[:, :, i] .= other_chains[i][chain_draws_1[:, :, i]]
+    for k in axes(chain_draws_1, 3)
+        chain_draws_1[:, :, k] .= other_chains[k][chain_draws_1[:, :, k]]
     end
     
     chain_draws_2 = generate_random_numbers(rng, iterations, iteration_generation, chains, S = 1:(length(chains) - 2));
-    for (i, j, k) in axes(chain_draws_2)
+    for i in axes(chain_draws_2, 1), j in axes(chain_draws_2, 2), k in axes(chain_draws_2, 3)
         chain_draws_2[i, j, k] = setdiff(other_chains[k], chain_draws_1[i, j, k])[chain_draws_2[i, j, k]]
     end
     
     return deMCMC_params(βs, acceptances, chain_draws_1, chain_draws_2)
 end
 
-function update_chain!(X, X_ll, de_params::deMCMC_params, ld, γ, it, gen, chain)
+function update_chain!(X, X_ld, de_params::deMCMC_params, ld, γ, it, gen, chain)
     r1 = select_element(de_params.chain_draws_1, it, gen, chain);
     r2 = select_element(de_params.chain_draws_2, it, gen, chain);
-    xₚ = X[chain] .+ γ .* (X[r1] .- X[r2]) .+ select_element(de_params.βs, it, gen, chain, :);
-    if (ld(xₚ) - ld(X[chain])) > select_element(de_params.acceptances, it, gen, chain)
-        X[chain] .= xₚ;
-        X_ll[chain] = ld(xₚ);
+    xₚ = X[chain, :] .+ γ .* (X[r1, :] .- X[r2, :]) .+ select_element(de_params.βs, it, gen, chain, :);
+    ld_xₚ = ld(xₚ);
+    if (ld_xₚ - X_ld[chain]) > select_element(de_params.acceptances, it, gen, chain)
+        X[chain, :] .= xₚ;
+        X_ld[chain] = ld_xₚ;
     end
 end
 
-function update_sample!(samples, sample_ll, X, X_ll, it)
-    samples[it] = X;
-    sample_ll[it] = X_ll;
-end
-
-function run_deMCMC(ld::Function, dim; n_its = 1000, n_thin = 1, n_chains = 10, γ = 0.1, β = 0.1, rng = Random.GLOBAL_RNG)
-    n_its = 1000; n_thin = 5; n_chains = 10; γ = 0.1; β = 0.1; rng = Random.GLOBAL_RNG;
+function run_deMCMC(ld::Function, dim; n_its = 1000, n_burn = 5000, n_thin = 1, n_chains = 10, γ = 0.1, β = 0.1, rng = Random.GLOBAL_RNG)
+    
     # pre deMCMC setup
     iterations = 1:n_its;
     iteration_generation = 1:n_thin;
@@ -76,18 +84,47 @@ function run_deMCMC(ld::Function, dim; n_its = 1000, n_thin = 1, n_chains = 10, 
     de_params = deMCMC_params(iterations, iteration_generation, chains, params, β, rng);
 
     # setup population with random initial values (improve this)
-    X = map(x -> (rand(rng, dim) .- 0.5) .* 2, chains);
-    X_ll = map(x -> ld(x), X);
-    
-    samples = Array{Array{Array{Float64, 1}, 1}, 1}(undef, n_its);
-    sample_ll = Array{Array{Float64, 1}, 1}(undef, n_its);
+    X = randn(rng, n_chains, dim);
+    X_ld = map(x -> ld(x), eachrow(X));
 
-    for it in iterations
-        for gen in iteration_generation; chain in chains;
-            update_chain!(X, X_ll, de_params, ld, γ, it, gen, chain)
+    #burn in run
+    if n_burn > 0
+        burns = 1:n_burn;
+        burn_de_params = deMCMC_params(burns, 1:1, chains, params, β, rng);
+        burn_samples, burn_sample_ld = setup_samples(burns, chains, params);
+        for it in burns
+            for chain in chains
+                update_chain!(X, X_ld, burn_de_params, ld, γ, it, 1, chain)
+            end
+            update_sample!(burn_samples, burn_sample_ld, X, X_ld, it);
         end
-        update_sample!(samples, sample_ll, X, X_ll, it);
     end
+
+    (prod(size(burn_de_params.acceptances)) + prod(size(de_params.acceptances)) * 0.250)/60/60
+
+    #sampling run
+    samples, sample_ld = setup_samples(iterations, chains, params);
+    for it in iterations
+        for gen in iteration_generation, chain in chains
+            update_chain!(X, X_ld, de_params, ld, γ, it, gen, chain)
+        end
+        update_sample!(samples, sample_ld, X, X_ld, it);
+    end
+
+    #format output
+    output = (
+        samples = samples,
+        ld = sample_ld
+    )
+    if n_burn > 0
+        output = (
+            output...,
+            burnt_samples = burn_samples,
+            burnt_ld = burn_sample_ld
+        )
+    end
+
+    return output
 end
 
 end
