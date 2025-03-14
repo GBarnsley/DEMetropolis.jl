fbd098d18969dbeb372529c4a5012b983ae6a523
GBarnsley <gregbarnsley@hotmail.co.uk>
2025-03-14 15:23:30 +0000

    add ability to give initial states for some chains

--- src/deMCMC.jl
+++ src/deMCMC.jl
@@ -1,6 +1,6 @@
 module deMCMC
 export run_deMCMC
-import Random, TransformedLogDensities, LogDensityProblems
+import Random, TransformedLogDensities, LogDensityProblems, Logging
 
 struct deMCMC_params
     βs::Array{Float64, 4}
@@ -74,14 +74,12 @@ function update_chain!(X, X_ld, de_params::deMCMC_params, ld, γ, it, gen, chain
     end
 end
 
-function run_deMCMC(ld::Function, dim; n_its = 1000, n_burn = 5000, n_thin = 1, n_chains = nothing, γ = nothing, β = 1e-4, rng = Random.GLOBAL_RNG)
-    
+function run_deMCMC_inner(ld, initial_state; n_its, n_burn, n_thin, n_chains, γ, β, rng)
+
+    dim = size(initial_state, 2);
+
     if isnothing(γ)
         γ = 2.38/sqrt(2*dim);
-    end
-
-    if isnothing(n_chains)
-        n_chains = dim * 2;
     end
 
     # pre deMCMC setup
@@ -91,8 +89,7 @@ function run_deMCMC(ld::Function, dim; n_its = 1000, n_burn = 5000, n_thin = 1, 
     params = 1:dim;
     de_params = deMCMC_params(iterations, iteration_generation, chains, params, β, rng);
 
-    # setup population with random initial values (improve this)
-    X = randn(rng, n_chains, dim);
+    X = copy(initial_state);
     X_ld = map(x -> ld(x), eachrow(X));
 
     #burn in run
@@ -133,7 +130,55 @@ function run_deMCMC(ld::Function, dim; n_its = 1000, n_burn = 5000, n_thin = 1, 
     return output
 end
 
-function run_deMCMC(ld::TransformedLogDensity; n_its = 1000, n_burn = 5000, n_thin = 1, n_chains = nothing, γ = nothing, β = 1e-4, rng = Random.GLOBAL_RNG)
+
+function run_deMCMC(ld::Function, initial_state::Array{Float64, 2}; n_its = 1000, n_burn = 5000, n_thin = 1, n_chains = nothing, γ = nothing, β = 1e-4, rng = Random.GLOBAL_RNG)
+
+    dim = size(initial_state, 2);
+
+    if isnothing(n_chains)
+        n_chains = size(initial_state, 1);
+    end
+
+    if n_chains <= dim
+        @warn "n_chains ≤ the number of variables, sampler will perform poorly"
+    end
+
+    #check initial state matches
+    n_initial_state_chains = size(initial_state, 1);
+    if n_initial_state_chains == n_chains
+        true_initial_state = copy(initial_state);
+    elseif n_initial_state_chains < n_chains
+        @warn "initial_state has fewer chains than n_chains, adding random chains"
+        true_initial_state = cat(initial_state, randn(rng, n_chains - n_initial_state_chains, dim), dims = 1);
+    else
+        @warn "initial_state has more chains than n_chains, removing chains at random"
+        true_initial_state = initial_state[Random.randperm(rng, n_initial_state_chains)[1:n_chains], :];
+    end
+
+    run_deMCMC_inner(ld, true_initial_state; n_its = n_its, n_burn = n_burn, n_thin = n_thin, n_chains = n_chains, γ = γ, β = β, rng = rng)
+end
+
+function run_deMCMC(ld::Function, dim::Int; n_its = 1000, n_burn = 5000, n_thin = 1, n_chains = nothing, γ = nothing, β = 1e-4, rng = Random.GLOBAL_RNG)
+    
+    if isnothing(n_chains)
+        n_chains = dim * 2;
+    end
+
+    #setup population with random initial values
+    initial_state = randn(rng, n_chains, dim);
+
+    run_deMCMC_inner(ld, initial_state; n_its = n_its, n_burn = n_burn, n_thin = n_thin, n_chains = n_chains, γ = γ, β = β, rng = rng)
+end 
+
+function run_deMCMC(ld::TransformedLogDensities.TransformedLogDensity, initial_state::Array{Float64, 2}; n_its = 1000, n_burn = 5000, n_thin = 1, n_chains = nothing, γ = nothing, β = 1e-4, rng = Random.GLOBAL_RNG)
+    function _ld_func(x)
+        LogDensityProblems.logdensity(ld, x)
+    end
+
+    run_deMCMC(_ld_func, initial_state; n_its = n_its, n_burn = n_burn, n_thin = n_thin, n_chains = n_chains, γ = γ, β = β, rng = rng)
+end
+
+function run_deMCMC(ld::TransformedLogDensities.TransformedLogDensity; n_its = 1000, n_burn = 5000, n_thin = 1, n_chains = nothing, γ = nothing, β = 1e-4, rng = Random.GLOBAL_RNG)
     function _ld_func(x)
         LogDensityProblems.logdensity(ld, x)
     end
