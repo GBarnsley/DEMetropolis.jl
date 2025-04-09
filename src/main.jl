570c15f0ef91f2293064fd2826bb388f6b72eeaa
GBarnsley <gregbarnsley@hotmail.co.uk>
2025-04-09 16:19:09 +0100

    update testing

--- src/main.jl
+++ src/main.jl
@@ -10,18 +10,20 @@ include("update_chain.jl")
 
 end
 
+#import Random, TransformedLogDensities, LogDensityProblems, Logging, ProgressMeter, LinearAlgebra, StatsBase, MCMCDiagnosticTools
 #using MCMCDiagnosticTools, Plots, Distributions, BenchmarkTools
 #function ld(x)
 #    # normal mixture
 #    sum(logpdf(MixtureModel(Normal[Normal(-5.0, 1), Normal(5.0, 1)], [1/3, 2/3]), x))
 #end
-#pars = 20;
+#pars = 10;
 #
-#output = deMCMC.run_deMCMC(ld, pars; n_its = 1000, n_burn = 100000, n_thin = 1, n_chains = 200, deterministic_γ = false, memory = true, parallel = true, check_chain_epochs = 5);
-##output = deMCMC.run_deMCMC_live(ld, pars; n_its = 1000, check_every = 5000, n_chains = 100, deterministic_γ = false, parallel = true, save_burnt = true);
+#output = deMCMC.run_deMCMC(ld, pars; 
+#    n_its = 1000, n_burn = 10000, n_thin = 1, n_chains = 200, deterministic_γ = false, memory = false, parallel = true, check_chain_epochs = 3
+#);
+#output = deMCMC.run_deMCMC_live(ld, pars; n_its = 1000, check_every = 10000, n_chains = 100, deterministic_γ = false, parallel = true, save_burnt = true);
 #
 #mean(output.samples, dims = (1, 2))
-#mean(output.samples, dims = (1, 2, 3))
 #
 #mean(MixtureModel(Normal[Normal(-5.0, 1), Normal(5.0, 1)], [1/3, 2/3]))
 #
@@ -31,7 +33,4 @@ end
 #)
 #plot(
 #    output.samples[:, :, 1]
-#)
-#plot(
-#    output.samples[:, :, 2]
 #)
