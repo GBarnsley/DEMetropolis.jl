6b3c83734ed0079f6a3bbee1d8bab3ec958146da
GBarnsley <gregbarnsley@hotmail.co.uk>
2025-04-28 14:35:47 +0100

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,27 +2,28 @@ using deMCMC
 using Test
 using TransformVariables, TransformedLogDensities, Random, Distributions
 
+#easy problem that uses all the updates
+function ld_normal(x)
+    sum(-(x .* x)/2)
+end
+n_pars = 2;
+ld = TransformedLogDensities.TransformedLogDensity(as(Array, n_pars), ld_normal);
+n_chains = 4;
+rng = Random.MersenneTwister(1234);
+initial_state = randn(n_chains, n_pars);
+sampler_scheme = sampler_scheme_multi(
+    [1.0, 1.0, 1.0, 1.0],
+    [
+        setup_de_update(ld, deterministic_γ = false),
+        setup_de_update(ld, deterministic_γ = true),
+        setup_snooker_update(deterministic_γ = false),
+        setup_snooker_update(deterministic_γ = true)
+    ]
+);
+
 @testset "composite sampler" begin
-    #easy problem that uses all the updates
-    function ld_normal(x)
-        sum(-(x .* x)/2)
-    end
-    n_pars = 2
-    ld = TransformedLogDensities.TransformedLogDensity(as(Array, n_pars), ld_normal)
-    rng = Random.MersenneTwister(1234);
     n_its = 10;
-    n_chains = 4;
     n_burnin = 10;
-    initial_state = randn(n_chains, n_pars);
-    sampler_scheme = sampler_scheme_multi(
-        [1.0, 1.0, 1.0, 1.0],
-        [
-            setup_de_update(ld, deterministic_γ = false),
-            setup_de_update(ld, deterministic_γ = true),
-            setup_snooker_update(deterministic_γ = false),
-            setup_snooker_update(deterministic_γ = true)
-        ]
-    );
     output_mem = composite_sampler(
         ld, n_its, n_chains, true, initial_state, sampler_scheme;
         save_burnt = true, rng = rng, n_burnin = n_burnin, parallel = false
@@ -50,43 +51,23 @@ end
 
 @testset "composite sampler until converged" begin
     #easy problem that uses all the updates
-    function ld_normal(x)
-        sum(-(x .* x)/2)
-    end
-    n_pars = 2
-    ld = TransformedLogDensities.TransformedLogDensity(as(Array, n_pars), ld_normal)
-    rng = Random.MersenneTwister(1234);
     epoch_size = 1000;
-    n_chains = 4;
-    warm_up_epochs = 10;
-    initial_state = randn(n_chains, n_pars);
-    sampler_scheme = sampler_scheme_multi(
-        [1.0, 1.0, 1.0, 1.0],
-        [
-            setup_de_update(ld, deterministic_γ = false),
-            setup_de_update(ld, deterministic_γ = true),
-            setup_snooker_update(deterministic_γ = false),
-            setup_snooker_update(deterministic_γ = true)
-        ]
-    );
+    warmup_epochs = 2;
     output = composite_sampler(
-        ld, n_its, n_chains, true, initial_state, sampler_scheme, deMCMC.R̂_stopping_criteria(1.5);
-        save_burnt = true, rng = rng, n_burnin = n_burnin, parallel = false
+        ld, epoch_size, n_chains, true, initial_state, sampler_scheme, deMCMC.R̂_stopping_criteria(1.5);
+        save_burnt = true, rng = rng, warmup_epochs = warmup_epochs, parallel = false
     )
-    @test size(output.samples) == (n_its, n_chains, n_pars)
-    @test size(output.ld) == (n_its, n_chains)
-    @test size(output.burnt_samples) == (n_its, n_chains, n_pars)
-    @test size(output.burnt_ld) == (n_its, n_chains)
+    output_its = size(output.samples, 1)
+    output_its_burnt = size(output.burnt_samples, 1)
+    @test size(output.samples) == (output_its, n_chains, n_pars)
+    @test size(output.ld) == (output_its, n_chains)
+    @test size(output.burnt_samples) == (output_its_burnt, n_chains, n_pars)
+    @test size(output.burnt_ld) == (output_its_burnt, n_chains)
     @test eltype(output.samples) == eltype(initial_state)
     @test eltype(output.ld) == eltype(initial_state)
 end
 
 @testset "regular deMCMC" begin
-    function ld_normal(x)
-        sum(-(x .* x)/2)
-    end
-    n_pars = 2
-    ld = TransformedLogDensities.TransformedLogDensity(as(Array, n_pars), ld_normal);
     double_dist = setup_de_update(
         ld;
         γ = Distributions.Normal(0.8, 1.2),
@@ -158,4 +139,22 @@ end
         deterministic_γ = false
     )
     @test isa(ran.γ, Distributions.Uniform)
+end
+
+@testset "test rng states" begin
+    #should give the same result
+    n_its = 100;
+    n_burnin = 100;
+    rng = Random.MersenneTwister(112);
+    output1 = composite_sampler(
+        ld, n_its, n_chains, false, initial_state, sampler_scheme;
+        save_burnt = true, rng = rng, n_burnin = n_burnin, parallel = false
+    )
+    rng = Random.MersenneTwister(112);
+    output2 = composite_sampler(
+        ld, n_its, n_chains, false, initial_state, sampler_scheme;
+        save_burnt = true, rng = rng, n_burnin = n_burnin, parallel = false
+    )
+
+    @test isequal(output1, output2)
 end
